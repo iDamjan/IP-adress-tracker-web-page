2997eb9fa9aebb4a7491ab61f262c411a21b3fdd
Damjan Stojanovski <damjan.stojanovski123@gmail.com>
2022-04-21 19:41:35 +0200

    Added HTML markup and basic CSS styles, generated the MAP with javaScript

---
+++ app.js
@@ -0,0 +1,15 @@
+const ipAdress = document.querySelector('.ip-address-content')
+const location = document.querySelector('.location-content')
+const timezone = document.querySelector('.timezone-content')
+const facility = document.querySelector('.isp-content')
+
+const map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
